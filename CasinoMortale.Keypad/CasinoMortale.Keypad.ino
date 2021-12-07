105a6b5dcde79b88ebf060c8cb0608adc47f8fbb
Bas Paap <baspaap@live.nl>
2021-12-07 22:07:19 +0100

    Add arduino project

---
+++ CasinoMortale.Keypad/CasinoMortale.Keypad.ino
@@ -0,0 +1,17 @@
+/*
+ Name:		CasinoMortale.ino
+ Created:	12/7/2021 10:04:58 PM
+ Author:	baspa
+*/
+
+// the setup function runs once when you press reset or power the board
+#include <Adafruit_Keypad_Ringbuffer.h>
+#include <Adafruit_Keypad.h>
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
